c34375b8319160fe0dbf79887a560f39f909de40
Andrea <Andrea2000728>
2023-02-22 22:06:28 +0800

    refactor: set ArrayLIst with type parameters

--- src/main/java/wordfrequency/WordFrequencyGame.java
+++ src/main/java/wordfrequency/WordFrequencyGame.java
@@ -52,9 +52,8 @@ public class WordFrequencyGame {
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
         for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
+                ArrayList<Input> arr = new ArrayList<>();
                 arr.add(input);
                 map.put(input.getValue(), arr);
             } else {
